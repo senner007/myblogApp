52a2d9ab6fc5bc816d07ca54485b13e5de663e4c
senner007 <nielshtg@hotmail.com>
2018-10-27 16:34:18 +0200

    implement proper user validation

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using Isopoh.Cryptography.Argon2;
 //using System.Threading.Tasks;
 //using Microsoft.AspNetCore.Authorization;
 //using Microsoft.AspNetCore.Cors;
@@ -33,12 +34,16 @@ namespace MyblogApp.Controllers
                 String credValue = header.ToString().Substring("Basic ".Length).Trim();
                 String usernameAndPassenc = Encoding.UTF8.GetString(Convert.FromBase64String(credValue));
                 String[] usernameAndPass = usernameAndPassenc.Split(":");
+                string username = usernameAndPass[0];
+                string password = usernameAndPass[1];
 
-                GetUser getUser = new GetUser();
+                
                 // check in db
                 // username : Admin, password : pass
                 // username : User, password : pass2
-                if ( usernameAndPass[0] == getUser.GetUserName(usernameAndPass[0]) && sha256_hash(usernameAndPass[1]) == getUser.GetUserPass(usernameAndPass[0])) 
+                
+                ValidateUser validateUser = new ValidateUser();
+                if ( validateUser.CheckUserAndPassword(username, password)) 
                 {
                     SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(AppSettingsClass.SecurityKey));
                     
@@ -77,29 +82,51 @@ namespace MyblogApp.Controllers
         // {
         // }
 
-        // Update to PBKDF2 : https://stackoverflow.com/questions/4181198/how-to-hash-a-password/10402129#10402129
-        public static String sha256_hash(string value)
-        {
-            StringBuilder Sb = new StringBuilder();
+        // // Update to PBKDF2 : https://stackoverflow.com/questions/4181198/how-to-hash-a-password/10402129#10402129
+        // public static String sha256_hash(string value)
+        // {
+        //     StringBuilder Sb = new StringBuilder();
 
-            using (var hash = SHA256.Create())            
-            {
-                Encoding enc = Encoding.UTF8;
-                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+        //     using (var hash = SHA256.Create())            
+        //     {
+        //         Encoding enc = Encoding.UTF8;
+        //         Byte[] result = hash.ComputeHash(enc.GetBytes(value));
 
-                foreach (Byte b in result)
-                    Sb.Append(b.ToString("x2"));
-            }
+        //         foreach (Byte b in result)
+        //             Sb.Append(b.ToString("x2"));
+        //     }
 
-            return Sb.ToString();
-        }
+        //     return Sb.ToString();
+        // }
+        // public static String argon2_hash(string value)
+        // {
+        //     return Argon2.Hash(value);
+        // }
 
     }
-    public class GetUser 
+    public class ValidateUser 
     {
         MyblogContext dbContext = new MyblogContext();
-        public string GetUserName(string user) => dbContext.Users.Where(w => w.Username == user).FirstOrDefault().Username;
-        public string GetUserPass(string user) => dbContext.Users.Where(w => w.Username == user).FirstOrDefault().Password;
+        public string GetUserName(string user) => dbContext.Users.Where(w => w.Username == user).FirstOrDefault()?.Username;
+        public string GetUserPass(string user) => dbContext.Users.Where(w => w.Username == user).Single().Password;
+
+        public bool CheckUserAndPassword (string user, string password) 
+        {
+            if (String.IsNullOrEmpty(user) || String.IsNullOrEmpty(password)) {  
+                return false;
+            }
+            if (GetUserName(user) != null) {
+                if (Argon2.Verify(GetUserPass(user), password)) return true;
+                return false;
+            }
+             // Dette er for at eksekvere argon2 hashalgoritmen uanset brugernavn. Responstiden forbliver derved ens.
+            CalculatePasswordFirst();
+            return false;
+        }
+        public void CalculatePasswordFirst() 
+        {
+            Argon2.Verify(dbContext.Users.Where(w => w.Id == 1).First().Password, "dummy");
+        }
     }
 
     
